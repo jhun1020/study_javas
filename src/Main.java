4184e3f4ac724ad7096d89437dcd78d19a240155
jhun1020 <jhun1020.kh@gmail.com>
2022-11-03 15:00:02 +0900

    백준

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,45 @@ import java.util.Scanner;
 
 public class Main {
 
-    public int plus(int input_first, int input_second) {
-        int output_plus = input_first + input_second;
-        return output_plus;
-    }
- 
+/* - 문제 파악  
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+ - 유추 파악
+A+B를 T큼 반복
+
+- 단어 선정
+입력 input
+출력 output
+합 plus
+
+*/
+
+public int plus(int input_first, int input_second) {
+int output_plus = input_first + input_second;
+    return output_plus;
+}
+
+
     public static void main(String[] args) {
         //입력
         Scanner scanner = new Scanner(System.in);
+        int input_T = scanner.nextInt();
+
+        for(int i=1; i<=input_T; i++){
         int input_first = scanner.nextInt();
         int input_second = scanner.nextInt();
-        scanner.close();
-
+        
         //처리
         Main main = new Main();
         int output_plus = main.plus(input_first, input_second);
-
+        
         //출력
-        System.out.println(output_plus);
+         System.out.println(output_plus);
+        }
+
+        scanner.close(); 
     }
 
 }
+
